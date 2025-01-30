6d05b1d955ed427bc35bcf8d03ee8131bae93962
Iver <iver.bolstad1@gmail.com>
2025-01-30 21:05:19 +0100

    mobile changes footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,8 +1,8 @@
 export const Footer = () => {
   return (
     <footer className="bg-[#333533] bg-opacity-75 text-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="flex-1">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center gap-4">
+        <div className="w-full md:flex-1">
           <ul className="list-disc list-inside">
             <li>Telefon: +47 975 58 707</li>
             <li>E-post: post@anleggsgartner1.no</li>
@@ -11,7 +11,7 @@ export const Footer = () => {
             <li>Org.nr: 924 688 769</li>
           </ul>
         </div>
-        <div className="flex-1 text-right">
+        <div className="w-full md:flex-1 text-center md:text-right">
           <p>&copy; {new Date().getFullYear()} - Anleggsgartner 1 AS</p>
           <p>Created by <a href="https://www.linkedin.com/in/iver-bye-bolstad-167292227/" target="_blank" rel="noopener noreferrer">Iver Bolstad</a></p>
         </div>
